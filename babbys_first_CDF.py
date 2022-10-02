447633a1608805db29fc2d7dc203ee0ac88cbb06
Mark Cash <mcash00e@gmail.com>
2022-10-01 19:17:02 -0500

    Yeehaw

--- babbys_first_CDF.py
+++ babbys_first_CDF.py
@@ -4,18 +4,22 @@ os.environ["CDF_LIB"] = "~/CDF/lib"
 from spacepy import pycdf
 from shutil import copyfileobj, copy
 from tempfile import NamedTemporaryFile
+from pytplot import tplot, cdf_to_tplot
+
+
+##Yee haw. No gods no kings no comments
 
 head = {}
-url =  "https://spdf.gsfc.nasa.gov/pub/data/psp/isois/merged/l2/summary/2022/psp_isois_l2-summary_20220101_v13.cdf"
+suffix=''
+varnames=[]
+prefix = 'psp_isois_'
+url =  "https://spdf.gsfc.nasa.gov/pub/data/psp/isois/merged/l2/summary/2018/psp_isois_l2-summary_20181006_v13.cdf"
 session = requests.Session()
-
 fsvr = session.get(url, stream=True, verify=False, headers = head)
-
 ftmp = NamedTemporaryFile(delete=False)
 
 with open(ftmp.name, 'wb') as f:
     copyfileobj(fsvr.raw, f)
-
 copy(ftmp.name, "testData.cdf")
 
 fsvr.close()
@@ -24,3 +28,10 @@ os.unlink(ftmp.name)
 
 cdf = pycdf.CDF('testData.cdf')
 print(cdf)
+
+testVars = cdf_to_tplot("testData.CDF", suffix=suffix, prefix=prefix, get_support_data=False, 
+                    varformat=None, varnames=varnames, notplot=False)
+
+print(testVars)
+
+tplot([prefix+"Electron_CountRate_ChanE", prefix+"HET_A_H_Rate_TS", prefix+"HET_A_Electrons_Rate_TS", prefix+"H_CountRate_ChanP_SP"])
